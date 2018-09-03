09ff583b5fddc5439191008101d0b2ec7656fcad
Loveforkeeps <loveforkeeps@foxmail.com>
2018-09-03 15:34:06 +0800

    init push

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # DopmainReputationQuery
-基于https://redqueen.tj-un.comAPI的即时域名信誉查询系统
+## About DopmainReputationQuery
+
+基于[威胁情报厂商服务API]( https://redqueen.tj-un.comAPI)的域名信誉查询系统
+
+
+
+## Installing
+
+```bash
+git clone https://github.com/Loveforkeeps/DopmainReputationQuery
+```
+
+根据API说明在config文件中填入相关参数
+
+
+
+## Recommended Python Version:
+
+The recommended version for Python is **2.7.x*
+
+
+
+## Using DopmainReputationQuery
+
+```shell
+# python domianRep.py -h
+
+域名信誉查询工具
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --version         show program's version number and exit
+  -f FILE, --file FILE  select a task file
+  -o OUTPUT, --output OUTPUT
+                        specify the output csv file path and name
+  -a, --all             output all query results
+```
+
+选项说明：
+
+* 必填：
+  * `-f` : 指定需要查询的域名文件，格式为每行一个域名
+* 可选：
+  * `-o` : 指定输出的csv文件名和路径，指定名称时不需要加`.csv`类型后缀，程序自动添加
+  * `--all` :  无论是否有查出结果，均输出在结果文件中，方便对比
+
+### Example:
+
+```
+# ./rep.py -f task.txt -o result/123
+01.android2-phone.ujint.cn
+02.bd-pcgame.xiazai24.com
+02.xiao2015-xyx-pcgame.guo96.com
+09.cdn678.com
+1104u.com
+149hk.com
+190sihu.com
+24248.qichelife.com
+2no.co
+3344uk.com
+4cj5qu70.top
+955.cc
+aa066.com
+aaagg6.com
+ad.ad4989.co.kr
+ad.ilikesponsorad.co.kr
+ahfree.net
+ahmediye.net
+a.ligatus.com
+althawry.org
+^C
+When Querying 20... User Termined!
+Run time is 0 minutes 1 seconds!
+
+```
+
+查询结果将输出在result目录中的123.csv文件中
---
+++ rep.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Erdog
+
+from com.aliyun.api.gateway.sdk import client
+from com.aliyun.api.gateway.sdk.http import request
+from com.aliyun.api.gateway.sdk.common import constant
+import json
+import sys,io
+import argparse
+import csv
+from pprint import pprint
+import datetime
+
+# 解决错误UnicodeEncodeError: 'ascii' codec can't encode characters in position 5-6
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# 接受用户端参数
+parser = argparse.ArgumentParser(description="域名信誉查询工具",add_help=True,version="Beta1.0")
+parser.add_argument('-f','--file',type=argparse.FileType('r'),required=True,help='select a task file')
+parser.add_argument('-o','--output',type=str,help='specify the output csv file path and name')
+parser.add_argument('-a','--all',action='store_true',help="output all query results")
+args = parser.parse_args()
+
+if not args.output:
+    args.output = args.file.name+".csv"
+else:
+    args.output = args.output+".csv"
+
+
+HOST = "https://api.tj-un.com"
+URL = "/v1/reputation"
+
+with io.open("config","r",encoding="utf8") as f:
+    try:
+        j = json.load(f)
+        APPKEY = str(j[u"Appkey"])
+        APPSECRET = str(j[u"Appsecert"])
+        TOKEN = str(j[u"Token"])
+    except:
+        print(u"config文件中参数异常！")
+        exit(0)
+
+
+cli = client.DefaultClient(app_key=APPKEY, app_secret=APPSECRET)
+
+req_post = request.Request(host=HOST, protocol=constant.HTTPS, url=URL, method="POST", time_out=120)
+
+def functime(func):
+    def wap(*args,**kw):
+       local_time = datetime.datetime.now()
+       func(*args, **kw)
+       times = (datetime.datetime.now() - local_time).seconds
+       print 'Run time is {} minutes {} seconds!'.format(times/60,times%60)
+    return wap
+
+def domainRep(tasklist):
+    """ 域名信誉查询,返回response content """
+    paload = {
+        "token": TOKEN,
+        "value": tasklist,
+        "type": "domain",
+        "struct": "domain_reputation"
+    }
+    req_post.set_body(paload)
+
+    req_post.set_content_type(constant.CONTENT_TYPE_FORM)
+
+    res = cli.execute(req_post)
+    # print(res[0])
+    if res[0] == 200:
+        return res[2]
+    else:
+        print("!!!!!")
+        print(res)
+        return 0
+
+def rep2csv(jsondata):
+    """ 解析信誉查询结果并保存为csv文件 """
+    try:
+        j=json.loads(jsondata)
+        # print(j["response_data"]["domain_reputation"])
+        domain_reputations = j["response_data"]["domain_reputation"]
+        with open(args.output, 'a') as f:
+            dw = csv.DictWriter(f, [u'domain',u'category', u'score',u'tag', u'timestamp'])
+
+            for i in domain_reputations:
+                # pprint(i)
+                row = {'domain':i['domain']}
+                if len(i['reputation']):
+                    for repu in i['reputation']:
+                        row.update(repu)
+                        row["tag"] = ",".join(repu['tag'])
+                        # pprint(row)
+                        dw.writerow(row)
+                else:
+                    if args.all:
+                        dw.writerow(row)
+    except ValueError as e:
+            print(u"解析失败:{}".format(e))
+    except Exception as e:
+        print(u"未知异常: {}".format(e))
+        raise
+        return 0
+
+def json_csv(data,filename):
+    """  将iocs的JSON数据转换为CSV """
+    # global SCORELEVEL
+    with open(filename, 'a') as f:
+        dw = csv.DictWriter(f, [u'domain',u'category', u'score',u'tag', u'timestamp'])
+        for row in data:
+            # print(row)
+            row.update(row['reputation'][0])
+            row.pop('reputation','hash')
+            # print(row)
+            dw.writerow(row)
+    return 0
+
+@functime
+def main():
+    try:
+        with open(args.output, 'w') as f:
+            dw = csv.DictWriter(f, [u'domain',u'category', u'score',u'tag', u'timestamp'])
+            dw.writeheader()
+        f = args.file.read().splitlines()
+        for i in range(0,len(f),10):
+            t = f[i:i+10]
+            print "\n".join(t)
+            rep2csv(domainRep(t))
+    except KeyboardInterrupt:
+        print("\nWhen Querying {}... User Termined! ".format(i+10))
+        
+                
+if __name__ == '__main__':
+    main()
